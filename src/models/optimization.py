ef794578255cbcd0c322f8fa4a74085d4edd85b3
rosiindahag <shina_ruki@yahoo.com>
2025-06-05 20:13:09 +0700

    modify bayes opt library

--- src/models/optimization.py
+++ src/models/optimization.py
@@ -4,8 +4,18 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from mne.decoding import CSP
 from sklearn.svm import SVC
-from bayes_opt import BayesianOptimization
+# from bayes_opt import BayesianOptimization
 
+from skopt import gp_minimize
+from skopt.space import Real
+from skopt.utils import use_named_args
+import numpy as np
+
+# Define the search space
+search_space = [
+    Real(0.0, 3.96, name='t_start'),  # 4.0 - 0.04
+    Real(1.0, 4.0, name='t_end')
+]
 
 best_score = -np.inf
 best_params = {}
@@ -30,20 +40,28 @@ def optimize_model(X_segmented, y):
         return cross_val_score(pipeline, X_segmented, y, cv=cv, scoring='accuracy').mean()
 
     study = optuna.create_study(direction='maximize')
-    study.optimize(optuna_objective, n_trials=20, show_progress_bar=True)
+    study.optimize(optuna_objective, n_trials=30, show_progress_bar=True)
     return study.best_params, study.best_value
 
-def run_bayesian_optimization(X_train, y_train, sfreq, max_iterations=100):
-    def objective(t_start, t_end):
-        global best_score, best_params
-        if t_end <= t_start:
-            return -1.0
+def run_bayesian_optimization(X_train, y_train, sfreq, acq_func, max_iterations=50):
+    global best_score, best_params
+
+    @use_named_args(search_space)
+    def objective(**params):
+        t_start = round(params['t_start'], 3)
+        t_end = round(params['t_end'], 3)
+
+        if t_end <= t_start or (t_end - t_start) <= 0.04:
+            return 1.0
+
         X_segmented = crop_eeg(X_train, t_start, t_end, sfreq)
+
         try:
             hyperparams, acc = optimize_model(X_segmented, y_train)
         except Exception:
-            return -1.0
+            return 1.0
 
+        global best_score, best_params
         if acc > best_score:
             best_score = acc
             best_params = {
@@ -52,16 +70,55 @@ def run_bayesian_optimization(X_train, y_train, sfreq, max_iterations=100):
                 'n_components': hyperparams['n_components'],
                 'C': hyperparams['C'],
                 'kernel': hyperparams['kernel'],
-                'accuracy': acc
+                'accuracy': acc,
+                'gamma': hyperparams.get('gamma', 'scale'),
+                'acq_func':acq_func
             }
-            best_params['gamma'] = hyperparams.get('gamma', 'scale')
-        return acc
 
-    optimizer = BayesianOptimization(
-        f=objective,
-        pbounds={'t_start': (0.0, 4.0 - 0.04), 't_end': (0.04, 4.0)},
-        verbose=2,
-        random_state=42
+        return 1.0 - acc
+    result = gp_minimize(
+        func=objective,
+        acq_func=acq_func,
+        dimensions=search_space,
+        n_calls=max_iterations,
+        n_initial_points=5,
+        random_state=42,
+        verbose=True
     )
-    optimizer.maximize(init_points=5, n_iter=max_iterations)
+
     return best_params
+
+# def run_bayesian_optimization(X_train, y_train, sfreq, max_iterations=50):
+#     def objective(t_start, t_end):
+#         global best_score, best_params
+#         if t_end <= t_start:
+#             return -1.0
+#         if (t_end - t_start) <= 0.04:
+#             return -1.0
+#         X_segmented = crop_eeg(X_train, t_start, t_end, sfreq)
+#         try:
+#             hyperparams, acc = optimize_model(X_segmented, y_train)
+#         except Exception:
+#             return -1.0
+
+#         if acc > best_score:
+#             best_score = acc
+#             best_params = {
+#                 't_start': t_start,
+#                 't_end': t_end,
+#                 'n_components': hyperparams['n_components'],
+#                 'C': hyperparams['C'],
+#                 'kernel': hyperparams['kernel'],
+#                 'accuracy': acc
+#             }
+#             best_params['gamma'] = hyperparams.get('gamma', 'scale')
+#         return acc
+
+#     optimizer = BayesianOptimization(
+#         f=objective,
+#         pbounds={'t_start': (0.0, 4.0 - 0.04), 't_end': (1.0, 4.0)},
+#         verbose=2,
+#         random_state=42
+#     )
+#     optimizer.maximize(init_points=5, n_iter=max_iterations)
+#     return best_params
